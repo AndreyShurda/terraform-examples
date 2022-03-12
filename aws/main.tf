04aff9d6d26bc32bbd1e2a3b60b1baec1a79a95d
Andrii Shurda <andrii.shurda@resmed.com>
2022-03-12 22:17:25 +0200

    Add tf files for digitalocean and aws

---
+++ aws/main.tf
@@ -0,0 +1,71 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0d527b8c289b4af7f"
+  instance_type = "t2.micro"
+  key_name      = "aws_fr"
+  count         = 5
+  user_data     = <<-EOL
+    #!/bin/bash
+      sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+      sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+      sudo apt update
+      sudo apt -y install docker-ce
+      docker run -d ghcr.io/opengs/uashield:master 512 true
+    EOL
+
+}
+
+resource "aws_security_group_rule" "default" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "sg-123456"
+}
+
+module "stop_ec2_instance" {
+  source                         = "diodonfrost/lambda-scheduler-stop-start/aws"
+  name                           = "ec2_stop"
+  cloudwatch_schedule_expression = "cron(0 1 * * ? *)"
+  schedule_action                = "stop"
+  autoscaling_schedule           = "false"
+  ec2_schedule                   = "true"
+  rds_schedule                   = "false"
+  cloudwatch_alarm_schedule      = "false"
+  scheduler_tag = {
+    key   = "tostop"
+    value = "true"
+  }
+}
+
+module "start_ec2_instance" {
+  source                         = "diodonfrost/lambda-scheduler-stop-start/aws"
+  name                           = "ec2_start"
+  cloudwatch_schedule_expression = "cron(0 5 * * ? *)"
+  schedule_action                = "start"
+  autoscaling_schedule           = "false"
+  ec2_schedule                   = "true"
+  rds_schedule                   = "false"
+  cloudwatch_alarm_schedule      = "false"
+  scheduler_tag = {
+    key   = "tostop"
+    value = "true"
+  }
+}
---
+++ digitalocean/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
+
+# Get ssh key
+data "digitalocean_ssh_key" "terraform" {
+  name = "terraform" //name of key of digitalOcean account
+}
+
+variable "do_token" {}
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a droplet
+resource "digitalocean_droplet" "ubuntu" {
+  image              = "docker-20-04"
+  name               = "ubuntu-with-docker-${count.index}"
+  region             = "fra1"
+  size               = "s-1vcpu-1gb" # https://developers.digitalocean.com/documentation/v2/#list-all-sizes
+  private_networking = true
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  count = "7"
+
+  user_data = <<-EOL
+    #!/bin/bash
+    docker run -d ghcr.io/opengs/uashield:master 512 true
+    EOL
+}
